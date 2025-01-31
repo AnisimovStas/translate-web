f3cd870cbda103953c9c97e9783e57dbe914cb98
SAnisimov <anisimovsa@apheco.ru>
2025-01-31 14:26:32 +0400

    добавлен allowed preview

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    preview:{
+      allowedHosts: ['feral.servebeer.com', 'localhost', '127.0.0.1']
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
